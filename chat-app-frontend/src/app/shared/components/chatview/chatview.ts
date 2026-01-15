dd6514cc7581bf663f61b7f74cdbb0a70a1673d3
Rebell180 <buchmann_marcel@web.de>
2026-01-15 23:12:18 +0100

    clean landingpage and encapsulate logic of api calls and content chatview component;  create chatview component and schow data via api calls with created chatservice

--- chat-app-frontend/src/app/shared/components/chatview/chatview.ts
+++ chat-app-frontend/src/app/shared/components/chatview/chatview.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal, signal } from '@angular/core';
+import { Chat } from '../../classes/Chat';
+import { ChatService } from '../../services/chat-service';
 
 @Component({
   selector: 'app-chatview',
@@ -8,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class Chatview {
 
+  protected newChat = signal<Chat>(new Chat());
+  
+  protected cs: ChatService = inject(ChatService);
+  readonly chats: Signal<Chat[]> = this.cs.chats;
+
+
+
+  sendMessage(): void {
+    console.log("sende " + this.newChat().name + " mit der Nachricht: " + this.newChat().message);
+    this.cs.postMessage(this.newChat());
+  }
+
+  updateName(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.newChat.update(u => ({ ...u, name: value }));
+  }
+
+  updateMessage(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.newChat.update(u => ({ ...u, message: value }));
+  }
 }
---
+++ chat-app-frontend/src/app/shared/services/chat-service.ts
@@ -0,0 +1,40 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chat } from '../classes/Chat';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap, timer } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+
+    private readonly apiUrl = 'http://localhost:8000/api/chat/';
+    readonly chats = toSignal(
+        timer(0, 3000).pipe(
+            switchMap(() => this.http.get<Chat[]>(this.apiUrl))
+        ),
+        { initialValue: [] }
+    );
+
+
+    constructor(private http: HttpClient) { 
+    }
+
+    postMessage(newChat: Chat) {
+      // TODO Try Catch
+      this.http.post('http://localhost:8000/api/chat/', {
+        name: newChat.name,
+        message: newChat.message
+      }).subscribe(response => {
+        console.log(response);
+      });
+    }
+
+
+
+
+
+
+
+}
